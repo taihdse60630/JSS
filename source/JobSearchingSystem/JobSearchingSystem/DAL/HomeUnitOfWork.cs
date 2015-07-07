9b03be610b71f892daf3947aef9ba5fad5557673
hakunnguyen <paulhenrichanel14@gmail.com>
2015-07-07 23:08:40 +0700

    Them HomeUnitOfWork va JobUnitOfWork

---
+++ source/JobSearchingSystem/JobSearchingSystem/DAL/HomeUnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JobSearchingSystem.Models;
+
+namespace JobSearchingSystem.DAL
+{
+    public class HomeUnitOfWork : UnitOfWork
+    {
+        public void getAllJob (HIndexViewModel model)
+        {
+            model.jobList = JobRepository.Get();
+        }
+        public void findJob(HIndexViewModel model)
+        {
+            var jobList = JobRepository.Get();
+            if (!String.IsNullOrEmpty(model.searchString))
+            {
+                jobList = jobList.Where(s => s.JobTitle.ToUpper().Contains(model.searchString.ToUpper()));
+            }
+            model.jobList = jobList;
+          
+        }
+        
+    }
+}
---
+++ source/JobSearchingSystem/JobSearchingSystem/DAL/JobUnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JobSearchingSystem.Models;
+
+namespace JobSearchingSystem.DAL
+{
+    public class JobUnitOfWork : UnitOfWork
+    {
+        public IEnumerable<JJobItem> FindJob(String searchString)
+        {
+            return (from j in this.JobRepository.Get()
+                    join c in this.CompanyInfoRepository.Get()
+                    on j.RecruiterID equals c.RecruiterID
+                    where c.IsVisible = true
+                    select new JJobItem()
+                    {
+                        JobID = j.JobID,
+                        RecruiterID = j.RecruiterID,
+                        JobTitle = j.JobTitle,
+                        LogoURL = c.LogoURL
+                    }
+                   ).AsEnumerable();
+        }
+    }
+}
