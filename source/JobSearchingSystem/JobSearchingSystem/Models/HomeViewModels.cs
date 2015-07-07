a623e6da1c832aeb44ba5d48e3c185983553a12a
hakunnguyen <paulhenrichanel14@gmail.com>
2015-07-07 23:09:17 +0700

    Hakun them HomeViewModels.cs va JobViewModels.cs

---
+++ source/JobSearchingSystem/JobSearchingSystem/Models/HomeViewModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobSearchingSystem.Models
+{
+    public class HIndexViewModel
+    {
+        public IEnumerable<Job> jobList { get; set; }
+        public String searchString { get; set; }
+    }
+}
---
+++ source/JobSearchingSystem/JobSearchingSystem/Models/JobViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobSearchingSystem.Models
+{
+    public class JJobItem 
+    {
+        public int JobID { get; set; }
+        public string RecruiterID { get; set; }
+        public string JobTitle { get; set; }
+        public string LogoURL { get; set; }
+    }
+    public class JFindViewModel
+    {
+        public String searchString { get; set; }
+        public IEnumerable<JJobItem> jJobItem { get; set; }
+    }
+}
